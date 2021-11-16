766aa8c986a6bdd5efafde416faecf08381e5b81
Maxim Starikov <maksim.starikov@sunshinesmile.de>
2021-11-16 20:26:58 +0100

    added influx db

--- importer/index.js
+++ importer/index.js
@@ -1,12 +1,26 @@
 import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+import Influx from 'influx';
 
 dotenv.config();
+const influx = new Influx.InfluxDB(process.env.INFLUXDB_URL);
 
 async function loadNames() {
     try {
         const response = await fetch('https://api.ton.sh/getCoinPrice');
         const names = await response.json();
         console.log(names);
+
+        influx.writePoints([
+            {
+                measurement: 'tonth',
+                tags: {
+                    unit: locationObj.rawtide.tideInfo[0].units,
+                    location: locationObj.rawtide.tideInfo[0].tideSite,
+                },
+                fields: { height: tidePoint.height },
+                timestamp: tidePoint.epoch,
+            }()
     } catch (e) {
         console.log(e)
     }
